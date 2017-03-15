36f327fe2de2911d5b9000e7e4eac282796fbf7a
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-03-15 10:04:31 +0900

    Press machine updated

--- CESAFunFun/Assets/Members/Irfan/PressMachine.cs
+++ CESAFunFun/Assets/Members/Irfan/PressMachine.cs
@@ -8,21 +8,13 @@ public class PressMachine : MonoBehaviour
     private float speed = 5;
     [SerializeField]
     private float backSpeed = 5;
+    [SerializeField]
+    private float backTime = 2;
 
     private Vector3 startPos;
     private bool actived = false;
 
     private float time;
-
-
-
-    //void OnCollisionEnter(Collision col)
-    //{
-    //    Debug.Log(col.gameObject.name);
-    //    actived = false;
-    //    if(col.gameObject.tag == "Player")
-    //    col.gameObject.active = false;
-    //}
 
     // Use this for initialization
     void Start ()
@@ -35,26 +27,17 @@ public class PressMachine : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        time++;
-        
+         
         if (Input.GetKey(KeyCode.Z))
             actived = true;
 
         if (Input.GetKey(KeyCode.B))
             actived = false;
 
-        //if (transform.position == Vector3.zero)
-        //    actived = false;
-
-        if(time >= 3 * 60)
+        if(time >= backTime * 60)
             actived = false;
-        
 
         MachineOn(actived);
-
-
-
-
 
     }
 
@@ -66,10 +49,15 @@ public class PressMachine : MonoBehaviour
         //    transform.position = Vector3.MoveTowards(transform.position, startPos, backSpeed * Time.deltaTime);
 
         if (on)
+        {
             transform.Translate(Vector3.down * speed * Time.deltaTime);
+            time++;
+        }
         else
+        {
             transform.position = Vector3.MoveTowards(transform.position, startPos, backSpeed * Time.deltaTime);
-
+            time = 0;
+        }
     }
 
 
