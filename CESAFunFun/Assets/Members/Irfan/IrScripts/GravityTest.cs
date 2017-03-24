4f461b532714aec75e8d5538da78bb603bef8604
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-03-24 16:42:53 +0900

    Floor Movement

---
+++ CESAFunFun/Assets/Members/Irfan/IrScripts/GravityTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityTest : MonoBehaviour {
+
+    [SerializeField]
+    private bool invertGravity = false;
+
+    private Rigidbody rg;
+	// Use this for initialization
+	void Start () {
+        rg = gameObject.GetComponent<Rigidbody>();
+		
+	}
+    void Update()
+    {
+        if(invertGravity)
+            rg.AddForce(-Physics.gravity, ForceMode.Acceleration);
+        else
+            rg.AddForce(Physics.gravity, ForceMode.Acceleration);
+
+    }
+}
---
+++ CESAFunFun/Assets/Members/Irfan/IrScripts/PressMachine.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressMachine : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+    [SerializeField]
+    private float backSpeed = 5;
+    [SerializeField]
+    private float backTime = 0.5f;
+
+    public bool _actived = false;
+
+    private Vector3 startPos;
+    private float time;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        startPos = new Vector3(transform.position.x, transform.position.y);
+        time = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(time >= backTime * 60)
+            _actived = false;
+
+        Debug.Log(time);
+
+        MachineOn(_actived);
+
+    }
+
+    public void MachineOn(bool on)
+    {
+        if (on)
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            time++;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos, backSpeed * Time.deltaTime);
+            time = 0;
+        }
+    }
+
+
+
+}
