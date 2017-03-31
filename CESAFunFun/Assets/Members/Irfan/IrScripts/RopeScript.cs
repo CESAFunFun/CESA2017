2bdfd3dac155c6fa8ffb6882d994f743896a7f8d
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-03-31 12:00:39 +0900

    Bridgeみたいな床

--- CESAFunFun/Assets/Members/Irfan/IrScripts/RopeScript.cs
+++ CESAFunFun/Assets/Members/Irfan/IrScripts/RopeScript.cs
@@ -6,7 +6,7 @@ public class RopeScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        GetComponent<CharacterJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+        GetComponent<HingeJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
