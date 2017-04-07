257e83c26dea2a1547dbe877b05513fdb2f5b419
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-04-07 14:22:46 +0900

    no message

--- CESAFunFun/Assets/Members/Irfan/IrScripts/DetectorScript.cs
+++ CESAFunFun/Assets/Members/Irfan/IrScripts/DetectorScript.cs
@@ -4,48 +4,70 @@ using UnityEngine;
 
 public class DetectorScript : MonoBehaviour {
 
-    public bool _actived;
+    public bool _actived = false;
+    public bool _stopped = true;
 
     [SerializeField]
     private float speed;
-
     [SerializeField]
-    private Vector3 targetPos;
+    private Vector3 endPosition;
+
+    //private Vector3 currentPosition;
     private Transform floorParent;
 
     private Vector3 startPos = new Vector3(0,0,0);
 
+    private List<GameObject> objs = new List<GameObject>();
+
     private void Start()
     {
         floorParent = transform.parent;
+        //currentPosition = floorParent.position;
     }
 
     private void Update()
     {
-        Debug.Log(floorParent.position);
-
         if (_actived)
-            floorParent.position = Vector3.MoveTowards(floorParent.position, targetPos, speed * Time.deltaTime);
-        else
-            floorParent.position = Vector3.MoveTowards(floorParent.position, Vector3.zero, speed * Time.deltaTime);
+        {
+            if (_stopped)
+                floorParent.position = Vector3.MoveTowards(floorParent.position, endPosition, speed * Time.deltaTime);
+            else
+                floorParent.position = Vector3.MoveTowards(floorParent.position, Vector3.zero, speed * Time.deltaTime);   
+        }
+
+        if(floorParent.position == Vector3.zero)
+        {
+            _actived = false;
+            _stopped = true;
+        }
+
+        Debug.Log(objs.Count);
+
+        objs.Clear();
     }
 
-    void OnTriggerStay(Collider col)
+    void OnTriggerEnter(Collider col)
     { 
         if (col.gameObject.tag == "Player")
         {
-
+            Debug.Log("Hit");
             _actived = true;
-            //_triggered = true;
-            Debug.Log("Triggered Stay");
-
-            //floorParent.position = Vector3.MoveTowards(floorParent.position, targetPos, speed * Time.deltaTime);
+            //_stopped = false;
         }
     }
-    void OnTriggerExit(Collider other)
+
+    private void OnTriggerStay(Collider other)
     {
-        Debug.Log("Exit");
-        _actived = false;
-        //floorParent.position = Vector3.MoveTowards(floorParent.position, Vector3.zero, speed * Time.deltaTime);
+        if (other.gameObject.tag == "Player" && !objs.Contains(other.gameObject))
+        {
+            objs.Add(other.gameObject);
+        }
     }
+    //void OnTriggerExit(Collider other)
+    //{
+    //    //Debug.Log("Exit");
+    //    //_actived = false;
+    //    _stopped = false;
+    //    //floorParent.position = Vector3.MoveTowards(floorParent.position, Vector3.zero, speed * Time.deltaTime);
+    //}
 }
