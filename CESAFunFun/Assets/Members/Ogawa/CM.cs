d125096184deec8c892c861e158a800b66c87899
TakayoshiOgawa <s152103>
2017-04-07 09:52:17 +0900

    ChildManagerの試作段階（CMとGMという命名）

---
+++ CESAFunFun/Assets/Members/Ogawa/CM.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject childPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public GameObject CreateChildCharacter(GameObject player, Vector3 position) {
+        // プレハブから生成
+        var child = Instantiate(childPrefab, position, Quaternion.identity);
+        // プレイヤーのデータを元に設定を行う(変数にしないと値が変えられない)
+        var character = child.GetComponent<RigidbodyCharacter>();
+        character._downGravity = player.GetComponent<RigidbodyCharacter>()._downGravity;
+        return child;
+    }
+
+    public void TrackCharacter(GameObject predator, GameObject target) {
+        predator.GetComponent<Tracking>()._target = target;
+    }
+
+}
